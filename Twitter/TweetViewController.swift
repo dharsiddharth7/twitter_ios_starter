705ac819f8b9858566d19c75fe9612c06e01b00a
Siddharth Dhar <dhar.siddharth1@gmail.com>
2020-02-10 18:54:39 -0500

    commit1-p2

---
+++ Twitter/TweetViewController.swift
@@ -0,0 +1,27 @@
+//
+//  TweetViewController.swift
+//  Twitter
+//
+//  Created by Siddharth Dhar on 2/10/20.
+//  Copyright © 2020 Dan. All rights reserved.
+//
+
+import UIKit
+
+class TweetViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+    @IBAction func cancel(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+
+    @IBAction func tweet(_ sender: Any) {
+    }
+}
